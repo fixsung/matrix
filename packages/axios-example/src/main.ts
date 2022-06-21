f8157d56e73be067b59ddc9b800341d204f9570b
宋玉震 <songyzh@dist.com.cn>
2022-06-21 23:11:31 +0800

    exprss init

--- packages/axios-example/src/main.ts
+++ packages/axios-example/src/main.ts
@@ -1,17 +1,11 @@
-/*
- * @Author: songyzh
- * @Date: 2022-06-20 17:33:04
- * @LastEditors: songyzh
- * @LastEditTime: 2022-06-21 10:47:53
- * @Description:
- */
-
 import express, { Request, Response } from 'express'
-
+import { green} from "kolorist"
 const app = express()
 
 app.get('/ping', (req: Request, res: Response) => {
   res.json({ msg: 'hello world' })
 })
 
-app.listen(3000)
+app.listen(3000,() => {
+  console.log(green(`Example app listening on port 3000`))
+})
